8ddc7dae12ee534852fa2c08c6c8f592234d4fb8
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-08-22 17:11:23 -0600

    Configurar PWM

--- Laboratorio 3/src/main.cpp
+++ Laboratorio 3/src/main.cpp
@@ -1,71 +1,107 @@
-//**********************************************************
+//*****************************************************************************
 //Universidad del Valle de Guatemala
 //BE3015: Electrónica Digital 2
-//**********************************************************
+//*****************************************************************************
 
-//**********************************************************
+//*****************************************************************************
 //Librerias
-//**********************************************************
-#include <Arduino.h>
+//*****************************************************************************
+#include <Arduino.h> 
 
-//**********************************************************
+//*****************************************************************************
 //Definicion etiquetas
-//**********************************************************
+//*****************************************************************************
 
-//Paso 1: selección de parámetros del PWM
-#define pwmChannel 5 // 16 canales 0-15
-#define freqPWM 5000 //frecuencia en Hz
-#define resolution 8 //1-16 bits de resolucion
-#define pinPWM 2     //GPIO 2 para tener la salida del PWM
+//Botones
+#define boton1 
+#define boton2
+#define boton3
+#define boton4
 
-//**********************************************************
+//Leds
+#define Led rojo
+#define Led verde
+#define Led azul
+
+//Parámetros PWM para led
+#define pwmChannelLed  2// 16 canales 0-15
+#define freqPWMLed 5000 //frecuencia en Hz
+#define resolutionPWMLed 8 //8 bits de resolucion (0-255)
+#define pinPWMLed     3//GPIO  para tener la salida del PWM
+
+
+//Parámetro PWM servo motor
+#define pwmChannelServo 1
+#define freqPWMServo 50 //frecuencia en Hz
+#define resolutionPWMServo 8 //8 bits de resolucion (0-255)
+#define pinPWMServo 5//GPIO  para tener la salida del PWM
+
+//*****************************************************************************
 //Prototipos de funcion
-//**********************************************************
-void configurarPWM(void);
+//*****************************************************************************
+void configurarPWMLed(void);
+void configurarPWMServo(void);
 
-//**********************************************************
+//*****************************************************************************
 //Varibles globales
-//**********************************************************
+//*****************************************************************************
 
-//**********************************************************
+//*****************************************************************************
 //ISR: interrupciones
-//**********************************************************
+//*****************************************************************************
 //void IRAM_ATTR ISR(){}
 
-//**********************************************************
+//*****************************************************************************
 //configuracion
-//**********************************************************
+//*****************************************************************************
 void setup()
 {
-  configurarPWM();
+  configurarPWMLed();
+  
 }
 
-//**********************************************************
+//*****************************************************************************
 //loop principal
-//**********************************************************
+//*****************************************************************************
 void loop()
 {
-  for (int dutycycle = 0; dutycycle < 256; dutycycle++)
+  /**
+   * for (int dutycycle = 0; dutycycle < 256; dutycycle++)
   {
-    ledcWrite(pwmChannel, dutycycle);
+    ledcWrite(pwmChannelLed, dutycycle);
     delay(10);
   }
 
   for (int dutycycle = 255; dutycycle > 0; dutycycle--)
   {
-    ledcWrite(pwmChannel, dutycycle);
+    ledcWrite(pwmChannelLed, dutycycle);
     delay(10);
-  }
+  }*/
 }
 
-//**********************************************************
+//*****************************************************************************
 //Función para configurar módulo PWM
-//**********************************************************
-void configurarPWM(void)
+//*****************************************************************************
+void configurarPWMLed(void)
 {
   //Paso 1: Configurar el modulo PWM
-  ledcSetup(pwmChannel, freqPWM, resolution);
+  ledcSetup(pwmChannelLed, freqPWMLed, resolutionPWMLed);
 
   //Paso 2: seleccionar en que GPIO tendremos nuestra señal PWM
-  ledcAttachPin(pinPWM, pwmChannel);
+  ledcAttachPin(pinPWMLed, pwmChannelLed);
+}
+
+void configurarPWMServo(void)
+{
+  //Paso 1: Configurar el modulo PWM
+  ledcSetup(pwmChannelServo, freqPWMServo, resolutionPWMServo);
+
+  //Paso 2: seleccionar en que GPIO tendremos nuestra señal PWM
+  ledcAttachPin(pinPWMServo, pwmChannelServo);
+}
+
+void configurarBoton1(void)
+{
+  ledcWrite(pwmChannelLed, 255);
+
 }
