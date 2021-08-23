59f958f6f4f13d5cce6e74b4a7187ea2e6f12a3f
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-08-23 01:58:52 -0600

    Parte 3 completada

--- Laboratorio 3/src/main.cpp
+++ Laboratorio 3/src/main.cpp
@@ -19,28 +19,28 @@
 #define boton4 19
 
 //Parámetro PWM servo motor
-#define pwmChannelServo 5
-#define freqPWMServo 50       //frecuencia en Hz
-#define resolutionPWMServo 16 //16 bits
-#define pinPWMServo 13        //GPIO  para tener la salida del PWM
+#define pwmChannelServo 5 
+#define freqPWMServo 50       
+#define resolutionPWMServo 16 
+#define pinPWMServo 13       
 
 //Parámetro PWM led Red
 #define pwmChannelLedR 2
-#define freqPWMLedR 5000    //frecuencia en Hz
-#define resolutionPWMLedR 8 //8 bits
-#define pinPWMLedR 12       //GPIO  para tener la salida del PWM
+#define freqPWMLedR 5000   
+#define resolutionPWMLedR 8 
+#define pinPWMLedR 12     
 
 //Parámetro PWM led Green
 #define pwmChannelLedG 1
-#define freqPWMLedG 5000    //frecuencia en Hz
-#define resolutionPWMLedG 8 //8 bits
-#define pinPWMLedG 27       //GPIO  para tener la salida del PWM
+#define freqPWMLedG 5000   
+#define resolutionPWMLedG 8 
+#define pinPWMLedG 27     
 
 //Parámetro PWM led Blue
 #define pwmChannelLedB 3
-#define freqPWMLedB 5000    //frecuencia en Hz
-#define resolutionPWMLedB 8 //8 bits
-#define pinPWMLedB 26       //GPIO  para tener la salida del PWM
+#define freqPWMLedB 5000   
+#define resolutionPWMLedB 8 
+#define pinPWMLedB 26     
 
 int contador = 0;
 int contadorBoton3 = 0;
@@ -67,6 +67,52 @@ void moverServo();
 //*****************************************************************************
 //ISR: interrupciones
 //*****************************************************************************
+void IRAM_ATTR ISRBoton1() //interrupción para botón 1 (Aumento)
+{
+  static unsigned long ultimo_tiempo_interrupcion3 = 0; //último tiempo de la interrupción
+  unsigned long tiempo_interrupcion3 = millis();        //tiempo actual de la interrupción
+
+  //Si la interrupcion dura menos de 200ms, asumir que es un rebote e ignorar
+  if (tiempo_interrupcion3 - ultimo_tiempo_interrupcion3 > 200)
+  {
+    contador -= 20; //aumenta 1 al contador de botón
+
+    if (contador == 180)
+    {
+      contador = 180;
+    }
+
+    else if (contador == 0)
+    {
+      contador = 0;
+    }
+  }
+  ultimo_tiempo_interrupcion3 = tiempo_interrupcion3; //actualiza el valor del tiempo de la interrupción
+}
+
+void IRAM_ATTR ISRBoton2() //interrupción para botón 1 (Aumento)
+{
+  static unsigned long ultimo_tiempo_interrupcion4 = 0; //último tiempo de la interrupción
+  unsigned long tiempo_interrupcion4 = millis();        //tiempo actual de la interrupción
+
+  //Si la interrupcion dura menos de 200ms, asumir que es un rebote e ignorar
+  if (tiempo_interrupcion4 - ultimo_tiempo_interrupcion4 > 200)
+  {
+    contador += 20; //aumenta 1 al contador de botón
+
+    if (contador == 180)
+    {
+      contador = 180;
+    }
+
+    else if (contador == 0)
+    {
+      contador = 0;
+    }
+  }
+  ultimo_tiempo_interrupcion4 = tiempo_interrupcion4; //actualiza el valor del tiempo de la interrupción
+}
+
 void IRAM_ATTR ISRBoton3() //interrupción para botón 1 (Aumento)
 {
   static unsigned long ultimo_tiempo_interrupcion1 = 0; //último tiempo de la interrupción
@@ -77,7 +123,7 @@ void IRAM_ATTR ISRBoton3() //interrupción para botón 1 (Aumento)
   {
     contadorBoton3++; //aumenta 1 al contador de botón
 
-    if (contadorBoton3 > 3) //si es mayor a 15 regresa el valor a cero
+    if (contadorBoton3 > 4) //si es mayor a 15 regresa el valor a cero
     {
       contadorBoton3 = 0;
     }
@@ -116,6 +162,8 @@ void setup()
   configurarPWMLedR();
   configurarPWMLedG();
   configurarPWMLedB();
+  configurarBoton1();
+  configurarBoton2();
   configurarBoton3();
   configurarBoton4();
 }
@@ -125,18 +173,7 @@ void setup()
 //*****************************************************************************
 void loop()
 {
-  if (digitalRead(boton1) == 0)
-  {
-    contador -= 2;
-    moverServo();
-  }
-
-  if (digitalRead(boton2) == 0)
-  {
-    contador += 2;
-    moverServo();
-  }
-
+  moverServo();
   encenderLeds();
 }
 
@@ -182,21 +219,21 @@ void configurarPWMLedB(void)
 //Botón para mover servo a la derecha
 void moverServo()
 {
-  if (contador == 180)
-  {
-    contador = 180;
-  }
+  int ang = (((contador / 180.0) * 2000) / 20000.0 * 65536.0) + 1634;
+  ledcWrite(pwmChannelServo, ang);
+  delay(15);
+}
 
-  else if (contador == 0)
-  {
-    contador = 0;
-  }
-  else
-  {
-    int ang = (((contador / 180.0) * 2000) / 20000.0 * 65536.0) + 1634;
-    ledcWrite(pwmChannelServo, ang); // tell servo to go to position in variable 'pos'
-    delay(15);
-  }
+void configurarBoton1(void)
+{
+  //me coloca una interrupción en el botón 1 (durante el cambio de alto a bajo)
+  attachInterrupt(digitalPinToInterrupt(boton1), ISRBoton1, RISING);
+}
+
+void configurarBoton2(void)
+{
+  //me coloca una interrupción en el botón 1 (durante el cambio de alto a bajo)
+  attachInterrupt(digitalPinToInterrupt(boton2), ISRBoton2, RISING);
 }
 
 void configurarBoton3(void)
@@ -209,6 +246,30 @@ void configurarBoton4(void)
 {
   //me coloca una interrupción en el botón 1 (durante el cambio de alto a bajo)
   attachInterrupt(digitalPinToInterrupt(boton4), ISRBoton4, RISING);
+}
+
+void modoServo(void)
+{
+  if (contador < 60)
+  {
+    ledcWrite(pwmChannelLedR, 0);
+    ledcWrite(pwmChannelLedG, 0);
+    ledcWrite(pwmChannelLedB, 255);
+  }
+
+  else if (contador > 60 && contador < 120)
+  {
+    ledcWrite(pwmChannelLedR, 0);
+    ledcWrite(pwmChannelLedG, 255);
+    ledcWrite(pwmChannelLedB, 0);
+  }
+
+  else if (contador > 120)
+  {
+    ledcWrite(pwmChannelLedR, 255);
+    ledcWrite(pwmChannelLedG, 0);
+    ledcWrite(pwmChannelLedB, 0);
+  }
 }
 
 void encenderLeds(void)
@@ -224,5 +285,9 @@ void encenderLeds(void)
   case 3:
     ledcWrite(pwmChannelLedB, contadorBoton4);
     break;
+  case 4:
+    modoServo();
+    break;
   }
 }
+
