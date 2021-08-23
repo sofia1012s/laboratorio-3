2af2fc0c94b525a2fdf81cc061f4cd443be8573c
Sofía Salguero <salguerosofia.1012@gmail.com>
2021-08-22 23:51:09 -0600

    Parte 1 Completa

--- Laboratorio 3/src/main.cpp
+++ Laboratorio 3/src/main.cpp
@@ -15,45 +15,11 @@
 //Botones
 #define boton1 23
 #define boton2 22
-<<<<<<< HEAD
 #define boton3 21
 #define boton4 19
 
-//Parámetros PWM para led rojo
-#define pwmChannelLedR 1    // 16 canales 0-15
-#define freqPWMLedR 5000    //frecuencia en Hz
-#define resolutionPWMLedR 8 //8 bits de resolucion (0-255)
-#define pinPWMLedR 12        //GPIO  para tener la salida del PWM
-
-//Parámetros PWM para led verde
-#define pwmChannelLedV 2    // 16 canales 0-15
-#define freqPWMLedV 5000    //frecuencia en Hz
-#define resolutionPWMLedV 8 //8 bits de resolucion (0-255)
-#define pinPWMLedV 14       //GPIO  para tener la salida del PWM
-
-//Parámetros PWM para led azul
-#define pwmChannelLedA 3    // 16 canales 0-15
-#define freqPWMLedA 5000    //frecuencia en Hz
-#define resolutionPWMLedA 8 //8 bits de resolucion (0-255)
-#define pinPWMLedA 27        //GPIO  para tener la salida del PWM
-=======
-#define boton3
-#define boton4
-
-//Leds
-#define Led rojo
-#define Led verde
-#define Led azul
-
-//Parámetros PWM para led
-#define pwmChannelLed 3    // 16 canales 0-15
-#define freqPWMLed 5000    //frecuencia en Hz
-#define resolutionPWMLed 8 //8 bits de resolucion (0-255)
-#define pinPWMLed 3        //GPIO  para tener la salida del PWM
->>>>>>> parent of 6f4cbc4 (Parte 1)
-
 //Parámetro PWM servo motor
-#define pwmChannelServo 0
+#define pwmChannelServo 3
 #define freqPWMServo 50       //frecuencia en Hz
 #define resolutionPWMServo 16 //16 bits
 #define pinPWMServo 13        //GPIO  para tener la salida del PWM
@@ -62,12 +28,9 @@ int contador = 0;
 //*****************************************************************************
 //Prototipos de funcion
 //*****************************************************************************
-void configurarPWMLedR(void);
-void configurarPWMLedV(void);
-void configurarPWMLedA(void);
 void configurarPWMServo(void);
 void configurarBoton1(void);
-void LedV();
+void configurarBoton2(void);
 void moverServo();
 
 //*****************************************************************************
@@ -89,9 +52,7 @@ void setup()
   pinMode(boton4, INPUT_PULLUP);
   pinMode(pinPWMServo, OUTPUT);
   configurarPWMServo();
-  configurarPWMLedR();
-  configurarPWMLedV();
-  configurarPWMLedA();
+
 }
 
 //*****************************************************************************
@@ -110,50 +71,11 @@ void loop()
     contador+=2;
     moverServo();
   }
-    for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
-    // changing the LED brightness with PWM
-    ledcWrite(pinPWMLedR, dutyCycle);
-    delay(15);
-  }
-
-  // decrease the LED brightness
-  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
-    // changing the LED brightness with PWM
-    ledcWrite(pinPWMLedR, dutyCycle);   
-    delay(15);
-  }
 }
 
 //*****************************************************************************
 //Función para configurar módulo PWM
 //*****************************************************************************
-void configurarPWMLedR(void)
-{
-  //Paso 1: Configurar el modulo PWM
-  ledcSetup(pwmChannelLedR, freqPWMLedR, resolutionPWMLedR);
-
-  //Paso 2: seleccionar en que GPIO tendremos nuestra señal PWM
-  ledcAttachPin(pinPWMLedR, pwmChannelLedR);
-}
-
-void configurarPWMLedV(void)
-{
-  //Paso 1: Configurar el modulo PWM
-  ledcSetup(pwmChannelLedV, freqPWMLedV, resolutionPWMLedV);
-
-  //Paso 2: seleccionar en que GPIO tendremos nuestra señal PWM
-  ledcAttachPin(pinPWMLedV, pwmChannelLedV);
-}
-
-void configurarPWMLedA(void)
-{
-  //Paso 1: Configurar el modulo PWM
-  ledcSetup(pwmChannelLedA, freqPWMLedA, resolutionPWMLedA);
-
-  //Paso 2: seleccionar en que GPIO tendremos nuestra señal PWM
-  ledcAttachPin(pinPWMLedA, pwmChannelLedA);
-}
-
 void configurarPWMServo(void)
 {
   //Paso 1: Configurar el modulo PWM
